da6ba94fd30dfcaf0ecf98b0a7a82e8156be572c
Devesh Lashkari <deveshlashkari@gmail.com>
2023-07-10 15:48:48 +0530

    updates

--- src/components/Vietnam/Vietnam.js
+++ src/components/Vietnam/Vietnam.js
@@ -6,20 +6,29 @@ const Vietnam = () => (
       style={{
         margin: 0,
         position: "absolute",
-        top: "50%",
-        left: "40%",
+        top: "200%",
+
         "-ms-transform": "translateY(-50%)",
         transform: "translateY(-50%)",
       }}
     >
-      <button
+      {/* <button
         style={{ textAlign: "center" }}
         onClick={() => {
           window.location.href = "https://www.youtube.com/watch?v=DenlS7VdATk";
         }}
       >
         click here for itinerary
-      </button>
+      </button> */}
+      <iframe
+        width="935"
+        height="526"
+        src="https://www.youtube.com/embed/DenlS7VdATk?autoplay=1"
+        title="Yeh Koi Randikhana Hai Meme Template"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
   </div>
 );
